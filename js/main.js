a8c92cd6daa035ba180de0685d2f1210ddf7872f
shreelimbkar <limbkarshree@gmail.com>
2021-12-04 01:47:18 +0000

    calculated bitcoin price

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const btnSubmit = document.querySelector(".btnSubmit");
 const txtDateTime = document.querySelector("input[name='txtDateTime']");
+const baseURL = "https://api.coingecko.com/api/v3/coins";
+let formattedNextDrawDateWithTime, bitCoinCurrentPrice, bitCoinValue;
 
 window.addEventListener("load", () => {
   const now = new Date();
   now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
   txtDateTime.defaultValue = now.toISOString().slice(0, -8);
+  getBitCoinCurrentPrice();
 });
 
 btnSubmit.addEventListener("click", (event) => {
@@ -12,6 +15,7 @@ btnSubmit.addEventListener("click", (event) => {
   if (!txtDateTime) return false;
   let nextDrawDate = new Date(getNextLottoDraw(txtDateTime.value));
 
+  // set draw time at 8:00 PM
   nextDrawDate.setHours(8);
   nextDrawDate.setMinutes(0);
   nextDrawDate.setMilliseconds(0);
@@ -21,61 +25,86 @@ btnSubmit.addEventListener("click", (event) => {
     .slice(0, 10)
     .split("-")
     .reverse()
-    .join("-")} 20:00`;
-  console.log("formattedNextDrawDate Date = ", formattedNextDrawDate);
+    .join("-")}`;
+  formattedNextDrawDateWithTime = `${formattedNextDrawDate} 20:00`;
+  drawDateBitCoinPrice(formattedNextDrawDate);
+  // console.log(
+  //   "formattedNextDrawDate Date = ",
+  //   formattedNextDrawDate,
+  //   bitCoinCurrentPrice
+  // );
 });
 
 const getNextLottoDraw = (inputDateTime) => {
   const nextLottoDrawDateTime = new Date(inputDateTime);
   let drawDate;
-  console.log("inputDateTime Wed = ", nextLottoDrawDateTime);
-  //   //   const getDay = inputDateTime.toString().split(" ")[0];
-  //   const getTime = parseInt(
-  //     nextLottoDrawDateTime.toString().split(" ")[4].split(":")[0]
-  //   );
   const selectedDay = parseInt(nextLottoDrawDateTime.getDay());
   const selectedHrs = parseInt(nextLottoDrawDateTime.getHours());
   const selectedMins = parseInt(nextLottoDrawDateTime.getMinutes());
-  console.log(
-    "selectedDay, selectedHrs, selectedMins",
-    selectedDay,
-    selectedHrs,
-    selectedMins
-  );
+  // console.log(
+  //   "selectedDay, selectedHrs, selectedMins",
+  //   selectedDay,
+  //   selectedHrs,
+  //   selectedMins
+  // );
 
   if (selectedDay <= 3 && selectedHrs <= 20) {
-    console.log("WED in if");
     if (selectedHrs == 20 && selectedMins > 0) {
-      console.log("WED in if if");
       drawDate = new Date(
         nextLottoDrawDateTime.getTime() +
           (6 - selectedDay) * (60 * 60 * 24 * 1000)
       );
     } else {
-      console.log("WED in else");
       drawDate = new Date(
         nextLottoDrawDateTime.getTime() +
           (3 - selectedDay) * (60 * 60 * 24 * 1000)
       );
     }
+  } else if (selectedDay <= 3 && selectedHrs > 20) {
+    console.log("in else");
+    drawDate = new Date(
+      nextLottoDrawDateTime.getTime() +
+        (3 - selectedDay) * (60 * 60 * 24 * 1000)
+    );
   }
   if (selectedDay > 3 && selectedDay <= 6 && selectedHrs <= 20) {
-    console.log("SAT in if");
     if (selectedHrs == 20 && selectedMins > 0) {
-      console.log("SAT in if if");
       drawDate = new Date(
         nextLottoDrawDateTime.getTime() +
           (selectedDay - 2) * (60 * 60 * 24 * 1000)
       );
     } else {
-      console.log("SAT in else");
       drawDate = new Date(
         nextLottoDrawDateTime.getTime() +
           (6 - selectedDay) * (60 * 60 * 24 * 1000)
       );
     }
+  } else if (selectedDay > 3 && selectedDay <= 6 && selectedHrs > 20) {
+    drawDate = new Date(
+      nextLottoDrawDateTime.getTime() +
+        (6 - selectedDay) * (60 * 60 * 24 * 1000)
+    );
   }
 
   // console.log("drawDate", drawDate);
   return drawDate;
+};
+
+const getBitCoinCurrentPrice = async () => {
+  const response = await fetch(`${baseURL}/bitcoin`);
+  const data = await response.json();
+  bitCoinCurrentPrice = data.market_data.current_price.eur;
+};
+
+const drawDateBitCoinPrice = async (drawDate) => {
+  const response = await fetch(`${baseURL}/bitcoin/history?date=${drawDate}`);
+  const data = await response.json();
+  bitCoinValue = parseFloat(
+    (data.market_data.current_price.eur * 100) / bitCoinCurrentPrice
+  ).toFixed(2);
+  // console.log(
+  //   "formattedNextDrawDateWithTime",
+  //   formattedNextDrawDateWithTime,
+  //   bitCoinValue
+  // );
 };
